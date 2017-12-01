8cd70cf9eb62739b94c61092b9c3e234467f8e6e
Admin <anshu.p@encoders.co.in>
2017-12-01 12:03:26 +0530

    bug fixed

--- app/src/main/java/com/example/debjit/myrecyclerview/ModuleActivity.java
+++ app/src/main/java/com/example/debjit/myrecyclerview/ModuleActivity.java
@@ -1,31 +1,31 @@
 package com.example.debjit.myrecyclerview;
 
 public class ModuleActivity {
-        // Has two properties one is text and another is image resource id
-        private String text;
-        private int Image;
+    // Has two properties one is text and another is image resource id
+    private String text;
+    private int Image;
 
-        //constructor for creating object with resources
-        public ModuleActivity(int image, String text){
-            this.Image = image;
-            this.text = text;
-        }
-
-        //to get the image resource id, call it
-        public int getImage() {
-            return Image;
-        }
-
-        public void setImage(int image) {
-            Image = image;
-        }
-
-        //to get the text, call it
-        public String getText() {
-            return text;
-        }
-
-        public void setText(String text) {
-            this.text = text;
-        }
+    //constructor for creating object with resources
+    public ModuleActivity(int image, String text){
+        this.Image = image;
+        this.text = text;
     }
+
+    //to get the image resource id, call it
+    public int getImage() {
+        return Image;
+    }
+
+    public void setImage(int image) {
+        Image = image;
+    }
+
+    //to get the text, call it
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
